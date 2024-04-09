72e3419bf64aff817c6d8a2e91c81b73632196f7
Dirlandia404 <dirlandiaoliveira@alu.ufc.br>
2024-04-09 19:13:24 -0300

    estudo metodos js

---
+++ teste.js
@@ -0,0 +1,21 @@
+const Array = ["elemento índice 0", "elemento índice 1"];
+const modifiedArray = Array.map((element) => element + " modificado");
+console.log(modifiedArray);
+// const ArrayNew = Array.slice();
+// console.log(ArrayNew);
+
+// Array.splice(0, 1);
+// console.log(Array);
+// Array.unshift("adicionando elemento ao inicio");
+// console.log(Array);
+
+// Array.shift();
+// console.log(Array);
+
+// Array.push("elemento adicionado");
+// console.log(Array);
+
+// console.log(Array.length);
+// console.log(Array);
+// Array.pop();
+// console.log(Array);
